d45ca5f8e61fd106911a945ea0d944ff22cb7a83
James <james@trueface.ai>
2019-04-25 12:28:09 -0700

    4

--- pywallet.py
+++ pywallet.py
@@ -17,10 +17,15 @@ class Pywallet(object):
         return data
 
     def posttransaction(self, otherparty, amt):
-        for node in nodes:
-            r = requests.post(node, data = {'key':'value'})
+        for node in self.nodes:
+            print(node)
+            #r = requests.post(node, data = {'key':'value'})
+
+    def addnode(self, node):
+        self.nodes.append(node)
 
 wallet = Pywallet()
-
-data = wallet.getrestjson(r'https://pyblockchain.herokuapp.com/chaintest')
-print(data)
+wallet.addnode(r'https://pyblockchain.herokuapp.com/chaintest')
+wallet.posttransaction(0,0)
+#data = wallet.getrestjson(r'https://pyblockchain.herokuapp.com/chaintest')
+#print(data)
